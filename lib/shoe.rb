6c724128be18c238360cf6cde935b51e2fc054d4
Karen C <chenyanhuan66@163.com>
2018-08-03 01:53:42 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,15 +1,15 @@
 # Make your shoe class here!
 class shoe
-  attr_accessor :brand
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
 
   def initialize(brand)
     @brand = brand
   end
 
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
+  def cobble
+    self.condition = "new"
+    puts "Your shoe is as good as new!"
   end
-
-
 
 end
