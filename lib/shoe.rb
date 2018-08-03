ce2f093cd988d826cc76888bc70350be6bff24e6
Karen C <chenyanhuan66@163.com>
2018-08-03 01:50:18 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,10 +1,10 @@
 # Make your shoe class here!
 class shoe
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :brand, :page_count, :genre
   attr_reader :title
 
-  def initialize(title)
-    @title = title
+  def initialize(brand)
+    @brand = brand
   end
 
   def turn_page
